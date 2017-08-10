2a43566bdd3653bad9031751a3a9a42cc65a03ae
gzq <1025174537@qq.com>
2017-08-10 23:07:13 +0800

    use better-scroll to let the food list scroll

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="(item,index) in goods" class="menu-item">
           <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
             {{item.name}}
@@ -10,7 +10,7 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
         <li v-for="item in goods" class="food-list">
           <h1 class="title">{{item.name}}</h1>
@@ -40,6 +40,8 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll';
+  // import food from '../../food/food';
   const ERR_OK = 0;
   // import axios from 'axios';
 
@@ -52,17 +54,75 @@
     data () {
       return {
         goods: []
+//        listHeight: [],
+//        scrollY: 0
       };
     },
+//    computed: {
+//      currentIndex () {
+//        for (let i = 0; i < this.listHeight.length; i++) {
+//          let height1 = this.listHeight[i];
+//          let height2 = this.listHeight[i + 1];
+//          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+//            this._followScroll(i);
+//            return i;
+//          }
+//        }
+//        return 0;
+//      }
+//    },
     created () {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
       this.$http.get('/api/goods').then((response) => {
         response = response.body;
         if (response.errno === ERR_OK) {
           this.goods = response.data;
-          console.log(this.goods);
+          this.$nextTick(() => { // 在这个回调函数中才能更新dom
+            this._initScroll();
+//            this._calculateHeight();
+          });
         }
       });
+    },
+    methods: {
+//      selectMenu(index, event) {
+//        if (!event._constructed) {
+//          return;
+//        }
+//        let foodList = this.$refs.foodList;
+//        let el = foodList[index];
+//        this.foodsScroll.scrollToElement(el, 300);
+//      },
+      _initScroll () {
+        this.meunScroll = new BScroll(this.$refs.menuWrapper, {
+          click: true
+        });
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+          click: true,
+          probeType: 3
+        });
+//        this.foodsScroll.on('scroll', (pos) => {
+//          // 判断滑动方向，避免下拉时分类高亮错误（如第一分类商品数量为1时，下拉使得第二分类高亮）
+//          if (pos.y <= 0) {
+//            this.scrollY = Math.abs(Math.round(pos.y));
+//          }
+//        });
+      }
+//      _calculateHeight() {
+//        let foodList = this.$els.foodList;
+//        let height = 0;
+//        this.listHeight.push(height);
+//        for (let i = 0; i < foodList.length; i++) {
+//          let item = foodList[i];
+//          height += item.clientHeight;
+//          this.listHeight.push(height);
+//        }
+//      },
+//      _followScroll(index) {
+//        let menuList = this.$refs.menuList;
+//        let el = menuList[index];
+//        this.meunScroll.scrollToElement(el, 300, 0, -100);
+//      }
     }
   };
 </script>
@@ -146,6 +206,7 @@
             font-size: 10px
             color: rgb(147, 153, 159)
           .desc
+            line-height: 12px
             margin-bottom: 8px
           .extra
             &.count
