9aeb0c363d44f7511e889039935f749ddabf8e2a
gzq <1025174537@qq.com>
2017-08-14 21:06:27 +0800

    menu的联动效果

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,8 @@
   <div class="goods">
     <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="(item,index) in goods" class="menu-item">
+        <li v-for="(item,index) in goods" class="menu-item" :class="{'current':currentIndex===index}"
+            ref="menuList" @click="selectMenu(index,$event)">
           <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
             {{item.name}}
@@ -12,7 +13,7 @@
     </div>
     <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list" ref="foodList">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item border-1px">
@@ -53,76 +54,78 @@
     },
     data () {
       return {
-        goods: []
-//        listHeight: [],
-//        scrollY: 0
+        goods: [],
+        listHeight: [], // 每个高度的数组
+        scrollY: 0
       };
     },
-//    computed: {
-//      currentIndex () {
-//        for (let i = 0; i < this.listHeight.length; i++) {
-//          let height1 = this.listHeight[i];
-//          let height2 = this.listHeight[i + 1];
-//          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
-//            this._followScroll(i);
-//            return i;
-//          }
-//        }
-//        return 0;
-//      }
-//    },
+    computed: {
+      currentIndex () { // 当前索引位置函数
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i]; // 当前高度
+          let height2 = this.listHeight[i + 1]; // 下一个高度
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+            // 开始结束范围
+            this._followScroll(i);
+            return i;
+          }
+        }
+        return 0;
+      }
+    },
     created () {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
       this.$http.get('/api/goods').then((response) => {
         response = response.body;
         if (response.errno === ERR_OK) {
           this.goods = response.data;
-          this.$nextTick(() => { // 在这个回调函数中才能更新dom
+          this.$nextTick(() => { // 这和函数很重要，在这个接口后做改变dom的操作，在这个回调函数后才能更新dom
             this._initScroll();
-//            this._calculateHeight();
+            this._calculateHeight(); // 联动时计算左侧高度
           });
         }
       });
     },
     methods: {
-//      selectMenu(index, event) {
-//        if (!event._constructed) {
-//          return;
-//        }
-//        let foodList = this.$refs.foodList;
-//        let el = foodList[index];
-//        this.foodsScroll.scrollToElement(el, 300);
-//      },
+      selectMenu(index, event) {
+//        console.log(index);
+        if (!event._constructed) { // 没有这个函数pc端将会派发两次事件,console.log(index)，将会执行两次
+          return;
+        }
+        let foodList = this.$refs.foodList;
+        let el = foodList[index];
+        this.foodsScroll.scrollToElement(el, 300);
+      },
       _initScroll () {
-        this.meunScroll = new BScroll(this.$refs.menuWrapper, {
+        this.meunScroll = new BScroll(this.$refs.menuWrapper, {// 在dom中定义ref="menuWrapper",然后在此处函数中可以用this.$ref.menuWrapper访问到dom
           click: true
         });
         this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
           click: true,
-          probeType: 3
+          probeType: 3 // 检测实时滚动位置
         });
-//        this.foodsScroll.on('scroll', (pos) => {
-//          // 判断滑动方向，避免下拉时分类高亮错误（如第一分类商品数量为1时，下拉使得第二分类高亮）
-//          if (pos.y <= 0) {
-//            this.scrollY = Math.abs(Math.round(pos.y));
-//          }
-//        });
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+          if (pos.y <= 0) {
+            this.scrollY = Math.abs(Math.round(pos.y));
+          }
+        });
+      },
+      _calculateHeight() {
+        let foodList = this.$refs.foodList;
+        let height = 0;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        }
+      },
+      _followScroll(index) {
+        let menuList = this.$refs.menuList;
+        let el = menuList[index];
+        this.meunScroll.scrollToElement(el, 300, 0, -100);
       }
-//      _calculateHeight() {
-//        let foodList = this.$els.foodList;
-//        let height = 0;
-//        this.listHeight.push(height);
-//        for (let i = 0; i < foodList.length; i++) {
-//          let item = foodList[i];
-//          height += item.clientHeight;
-//          this.listHeight.push(height);
-//        }
-//      },
-//      _followScroll(index) {
-//        let menuList = this.$refs.menuList;
-//        let el = menuList[index];
-//        this.meunScroll.scrollToElement(el, 300, 0, -100);
-//      }
     }
   };
 </script>
@@ -146,6 +149,14 @@
         width: 56px
         line-height: 14px
         padding: 0 12px
+        &.current
+          position:relative
+          margin-top: -1px
+          z-index: 10
+          background: #fff
+          .text
+            border-none()
+            font-weight: 700
         .icon
           display: inline-block;
           width: 12px;
